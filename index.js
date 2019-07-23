200051f016eb07084e054b1c7462179ab93f8479
green2onion <green2onion@users.noreply.github.com>
2019-07-23 23:47:39 +0800

    test fs

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const http = require('http');
 const express = require('express');
 const app = new express();
 
+var fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(8000);
+});
 
 app.get('/', function (request, response) {
-    response.sendFile('index.html');
+    response.sendFile('./index.html');
 })
 
 
