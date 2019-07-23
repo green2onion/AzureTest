2625cc70f14dc60203602c693106a3c27db54368
green2onion <green2onion@users.noreply.github.com>
2019-07-24 01:46:15 +0800

    i actually dont need the createserver stuff

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const http = require('http');
 const express = require('express');
 const app = new express();
 var path = require('path');
-
+var url = 'placeholder';
 const port = process.env.PORT || 1337;
 
 
-const server = http.createServer(app).listen(port, function (err) {
+
+/*const server = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end();
+}).listen(port, function (err) {
+
     if (err) {
         console.log(err);
     }
     else {
-        console.log("HTTP Server on http://localhost:%s", port)
+        console.log("HTTP Server on %s:%s", http, port)
     }
-})
-
+});  I actually don't need any of this stuff?? */
 
 app.get('/', function (request, response) {
     response.sendFile('index.html', { root: __dirname });
+}).listen(port, function (err) {
+
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("HTTP Server on %s:%s", url, port)
+    }
 });
+
+
+
+
