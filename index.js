0cbd88d0dfd2ea40dd04b4c67256bdf3653bec1d
green2onion <green2onion@users.noreply.github.com>
2019-07-24 00:50:17 +0800

    html hello world is working now

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const http = require('http');
 const express = require('express');
 const app = new express();
+var path = require('path');
 
-var fs = require('fs');
+const port = process.env.PORT || 1337;
 
 
-fs.readFile('./index.html', function (err, html) {
+const server = http.createServer(app).listen(port, function (err) {
     if (err) {
-        throw err;
+        console.log(err);
     }
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(8000);
-});
-
-app.get('/', function (request, response) {
-    response.sendFile('./index.html');
+    else {
+        console.log("HTTP Server on http://localhost:%s", port)
+    }
 })
 
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World!");
+app.get('/', function (request, response) {
+    response.sendFile('index.html', { root: path.join(__dirname, '../AzureTest') });
 });
-
-const port = process.env.PORT || 1337;
-server.listen(port);
